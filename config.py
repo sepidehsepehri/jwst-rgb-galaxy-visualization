71413352fb793e73a5f43e97cf9c3edf1f02bf15
Sepideh Sepehri <sepidehsepehri75@gmail.com>
2026-02-23 15:35:51 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+target_group = "blue" # or red , whatever you like :)
+companion_type_filter = "blue"
+
+z_range = (2.0, 8.0)
+
+cutout_arcsec = 8
+circle_radius_arcsec = 0.6
+
+mosaic_paths = {
+    'F115W': 'data/F115W.fits',
+    'F277W': 'data/F277W.fits',
+    'F444W': 'data/F444W.fits',
+}
